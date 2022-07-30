14a4acdee46c96c21fbd64eb144c7afd7e04d1da
JennyGlover <81665624+JennyGlover@users.noreply.github.com>
2022-07-30 14:11:07 -0400

    Added forecast function

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp){
     
 }
 
-function displayForecast(){
+function displayForecast(response){ 
+    console.log(response.data.daily)   
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
@@ -49,7 +50,7 @@ function displayForecast(){
                 </div>
     `;
     }
-    
+
     )
    
    
@@ -61,7 +62,14 @@ function displayForecast(){
    
 }
 
-function displayTenperature(response){
+function getForecast (coordinates){
+    let apiKey = "653f21a1a63480b1daa3161cac712581";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+    
+}
+
+function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
@@ -80,13 +88,16 @@ function displayTenperature(response){
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+    
+    getForecast(response.data.coord);
+
 
 }
 
 function search(city){
     let apiKey = "653f21a1a63480b1daa3161cac712581";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTenperature);
+    axios.get(apiUrl).then(displayTemperature);
 
 }
 
@@ -118,7 +129,6 @@ function displayCelciusTemperature(event){
 }
 
 let celciusTemperature = null;
-displayForecast();
 
 let form = document.querySelector("#sear-form");
 form.addEventListener("submit", handleSubmit);
